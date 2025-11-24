438a931d2d2e958ca55074c626c3b8cf1130cf52
MonsterGuti <gogulanovmartin@gmail.com>
2025-11-24 16:24:27 +0200

    working with a new ORM - sqlalchemy

---
+++ Python ORM/SQLalchemy project/models.py
@@ -0,0 +1,26 @@
+from sqlalchemy import Column, Integer, String, Text
+from sqlalchemy.orm import declarative_base, relationship
+
+Base = declarative_base()
+
+
+class Recipe(Base):
+    __tablename__ = 'recipes'
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    ingredients = Column(Text, nullable=False)
+    instructions = Column(Text, nullable=False)
+    chef = relationship(
+        'Chef',
+        back_populates='recipes'
+    )
+
+
+class Chef(Base):
+    __tablename__ = 'chefs'
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    Recipes = relationship(
+        'Recipe',
+        back_populates='chef'
+    )
